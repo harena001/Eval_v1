eccf507454c248b757773f29ed0d2d4e70ce090e
harena <iharylovanomena@gmail.com>
2023-07-16 18:50:20 +0300

    Premier commit and push

---
+++ src/test/java/com/harena/eval_v1/EvalV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harena.eval_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
